acf0737e195f4da2e27cea55adeeb2505d2ecde6
Timotei Iancu <timoteimarius.19@gmail.com>
2024-02-19 21:18:03 +0200

    Initial commit

---
+++ src/app/components/connect-server-page/connect-server-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connect-server-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './connect-server-page.component.html',
+  styleUrl: './connect-server-page.component.scss'
+})
+export class ConnectServerPageComponent {
+
+}
